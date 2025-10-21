81bac44aa89fb73d4722794af4fed6cb206ecc19
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2025-10-20 23:32:07 -0300

    arrumando comportamento esquisito nos redirecionamentos pós login

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard, FlatList, S
 import { useTheme } from '@/providers/ThemeModeProvider/ThemeModeProvider';
 import { LocaleContext } from '@/providers/LocaleProvider/LocaleProvider';
 import Button from '@/components/Button/Button';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { UserCard } from './components/UserCard';
 import { Album } from '@/components/Album';
 import { Ionicons } from '@expo/vector-icons';
@@ -113,9 +113,17 @@ export default function HomeScreen({ navigation }: any) {
     navigation.navigate('UserProfileScreen', { userId });
   };
 
+  const insets = useSafeAreaInsets();
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={[styles.wrapper, { backgroundColor: theme.highLight }]}>
+      <View style={[
+        styles.wrapper,
+        {
+          backgroundColor: theme.highLight,
+          paddingTop: insets.top
+        }
+      ]}>
         <View style={[styles.headContainer, { borderColor: theme.grey5 }]}>
           <Text style={[styles.title, { color: theme.primary100 }]}>{homeLocale.title}</Text>
 
@@ -244,7 +252,7 @@ export default function HomeScreen({ navigation }: any) {
             </View>
           </View>
         </ScrollView>
-      </SafeAreaView>
+      </View>
     </TouchableWithoutFeedback>
   );
 }
