9d512625ea81dbb9dccd1c76b761281daf989a77
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2026-02-20 00:08:01 -0300

    update socket to send token as a param

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,11 +1,16 @@
 import { urlApi } from "@/fakeenv";
 import { io, Socket } from "socket.io-client";
+import useStore from "../store";
 
 let socket: Socket | null = null;
 
 export const connectSocket = (userId: number | string) => {
   if (!socket) {
+    const state = useStore.getState();
+    const token = state.login.token;
+    
     socket = io(urlApi, {
+      auth: { token },
       query: { userId },
       transports: ["websocket"],
     });
